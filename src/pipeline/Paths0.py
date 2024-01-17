dd89269396f488f62edd101544bdeb56a47d3365
Joost de Folter <folterj@gmail.com>
2024-01-17 23:29:16 +0100

    Implemented efficient map-based paths

--- src/pipeline/Paths0.py
+++ src/pipeline/Paths0.py
@@ -3,27 +3,25 @@ from imageio.v3 import imwrite
 import math
 import numpy as np
 import os
+from scipy.spatial.distance import cdist
+from sklearn.neighbors import KDTree
 from tqdm import tqdm
 
 from src.file.plain_csv import export_csv_simple
 from src.util import ensure_out_path
 
 
-class PathLink:
-    def __init__(self, label, position1, position2, created):
+class PathNode:
+    def __init__(self, label, position, time):
         self.label = label
-        self.position1 = position1
-        self.position2 = position2
-        self.created = created
+        self.position = position
+        self.created = time
         self.total_use = 0
-        self.used = False
 
     def update_use(self, time):
         self.total_use += time
-        if not self.used:
-            self.used = True
 
-    def draw(self, image, position1, position2, time):
+    def draw(self, image, time):
         power = 3
         scale = self.total_use / (time + 1)
         if scale > 0:
@@ -32,22 +30,38 @@ class PathLink:
             col_scale = 0
         col_scale = np.clip(col_scale, 0, 1)
         color_value = [int(np.round(col_scale * 255))]
-        cv.line(image, position1, position2, color_value, 1, cv.LINE_AA)
+        position = np.round(self.position).astype(int)
+        cv.drawMarker(image, position, color_value, cv.MARKER_CROSS, 2, 1)
 
     def to_dict(self):
-        return {'x1': self.position1[0], 'y1': self.position1[1],
-                'x2': self.position2[0], 'y2': self.position2[1],
+        return {'label': self.label, 'x': self.position[0], 'y': self.position[1],
                 'created': self.created, 'total_use': self.total_use}
 
     def __str__(self):
         return str(self.to_dict())
 
 
+class PathLink:
+    def __init__(self, node1, node2):
+        self.node1 = node1
+        self.node2 = node2
+
+    def draw(self, image):
+        color = (127, 127, 127)
+        position1 = np.round(self.node1.position).astype(int)
+        position2 = np.round(self.node2.position).astype(int)
+        cv.line(image, position1, position2, color, 1, cv.LINE_AA)
+
+    def __str__(self):
+        return str(self.node1) + ' - ' + str(self.node2)
+
+
 class Paths:
     def __init__(self):
-        self.links = {}
+        self.nodes = []
+        self.node_positions = []
+        self.links = []
         self.next_label = 0
-        self.map = np.zeros((0, 0), dtype=np.float32)
         self.vidwriter = None
 
     def run(self, datas, features, params, general_params):
@@ -56,7 +70,6 @@ class Paths:
         self.features = features
         self.image_size = general_params['image_size']
         self.node_distance = params['node_distance']
-        node_distance = self.node_distance
         base_dir = general_params['base_dir']
         self.output = os.path.join(base_dir, params['output'])
         ensure_out_path(self.output)
@@ -66,9 +79,6 @@ class Paths:
         ensure_out_path(self.video_output)
         self.video_output_fps = params['video_output_fps']
         self.frame_interval = params['frame_interval']
-
-        map_size = np.divide(self.image_size, node_distance).astype(int) + 1
-        self.map = np.zeros(np.flip(map_size), dtype=np.float32)
 
         all_frames0 = set()
         min_frame = None
@@ -87,18 +97,15 @@ class Paths:
             all_frames0.update(data.frames)
         all_frames0 = sorted(all_frames0)
 
-        last_track_map_position = {}
+        last_track_nodes = {}
         for framei, frame in enumerate(tqdm(all_frames0)):
+            self.distance_pre_calc()
             for tracki, data in enumerate(datas):
                 if frame in data.frames:
                     position = data.position[frame]
-                    map_position = np.round(np.divide(position, node_distance)).astype(int)
-                    if tracki in last_track_map_position and not np.all(map_position == last_track_map_position[tracki]):
-                        self.map[map_position[1], map_position[0]] += framei
-                        last_position = last_track_map_position[tracki]
-                        self.update_link(last_position, map_position, framei)
-                    last_track_map_position[tracki] = map_position
-
+                    last_node = last_track_nodes.get(tracki)
+                    node = self.get_node(position, framei, last_node)
+                    last_track_nodes[tracki] = node
             if framei % self.frame_interval == 0:
                 self.save(framei)
                 self.draw(framei)
@@ -108,39 +115,69 @@ class Paths:
 
         return out_features
 
-    def update_link(self, last_position, position, time):
-        key = str(last_position[0]) + ',' + str(last_position[1]) + '-' + str(position[0]) + ',' + str(position[1])
-        link = self.links.get(key)
-        if key not in self.links:
-            self.links[key] = PathLink(self.next_label, last_position, position, time)
-            self.next_label += 1
+    def get_node(self, position, time, last_node):
+        node = None
+        node_distance = self.node_distance
+
+        if last_node is not None:
+            distance = math.dist(last_node.position, position)
+            if distance < node_distance / 2:
+                node = last_node
+
+        if node is None:
+            closest_node, distance = self.find_closest_node(position)
+            if closest_node is not None and distance < node_distance:
+                node = closest_node
+                node.update_use(time)
+
+        if node is None:
+            node = self.create_node(position, time, last_node)
+            node.update_use(time)
+        return node
+
+    def distance_pre_calc(self):
+        if self.node_positions:
+            self.position_tree = KDTree(self.node_positions)
+
+    def find_closest_node(self, position):
+        if self.node_positions:
+            #distance, index = calc_distance_cdist(position, self.node_positions)
+            distance, index = self.position_tree.query([position])
+            distance = distance[0][0]
+            index = index[0][0]
+            return self.nodes[index], distance
         else:
-            link.update_use(time)
+            return None, None
+
+    def create_node(self, position, time, linked_node=None):
+        label = self.next_label
+        self.next_label += 1
+        node = PathNode(label, position, time)
+        self.node_positions.append(position)
+        self.nodes.append(node)
+        if linked_node is not None:
+            self.links.append(PathLink(linked_node, node))
+        return node
 
     def save(self, framei):
         filename = self.output.format(frame=framei)
         output_data = {}
-        for link in self.links.values():
-            if link.used:
-                for key, value in link.to_dict().items():
-                    if key not in output_data:
-                        output_data[key] = []
-                    output_data[key].append(value)
+        for node in self.nodes:
+            for key, value in node.to_dict().items():
+                if key not in output_data:
+                    output_data[key] = []
+                output_data[key].append(value)
         export_csv_simple(filename, output_data)
 
     def draw(self, framei):
         if self.image_output is not None or self.video_output is not None:
+            shape = list(np.flip(self.image_size))
+            image = np.zeros(shape, dtype=np.uint8)
+            for node in self.nodes:
+                node.draw(image, framei)
             #for link in self.links:
-            #    if link.used:
-            #       link.draw(image, framei)
-
-            power = 3
-            image0 = self.map / (framei + 1)
-            image = 1 + (np.log10(image0, where=image0 > 0) - 2) / power  # log: 1(E0) ... 1E-[power]
-            image[image0 == 0] = 0
-            image = np.round(np.clip(image, 0, 1) * 255).astype(np.uint8)
+            #    link.draw(image)
             image = cv.applyColorMap(image, cv.COLORMAP_HOT)
-
             if self.image_output is not None:
                 image_filename = self.image_output.format(frame=framei)
                 rgb_image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
@@ -149,3 +186,10 @@ class Paths:
                 if self.vidwriter is None:
                     self.vidwriter = cv.VideoWriter(self.video_output, -1, self.video_output_fps, self.image_size)
                 self.vidwriter.write(image)
+
+
+def calc_distance_cdist(target, references):
+    distances = cdist([target], references)
+    index = distances.argmin()
+    distance = distances[0][index]
+    return distance, index
